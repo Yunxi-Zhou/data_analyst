e349050ac0b5113caf6dc25d3f20df32a6b49fc4
Yunxi-Zhou <zhouyunxi1115@gmail.com>
2025-08-13 13:41:52 -0400

    read csv file and set the max rows in system

---
+++ 6_readcsv.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+class ReadCSV:
+    def __init__(self):
+        self.sep = "----------------------"
+        self.data = 'data/customers-100.csv'
+
+    def ini_read(self,data):
+        print(self.sep)
+        return pd.read_csv(data)
+    
+    def set_row(self, nums):
+        pd.options.display.max_rows = nums
+    
+    def to_str(self):
+        return self.ini_read(self.data)
+    
+    def system_max(self):
+        # check the system's maximum rows with the statements
+        print(self.sep)
+        return pd.options.display.max_rows
+    
+    def max_rows(self):
+        self.set_row(9999)
+        return self.ini_read(self.data)
+
+
+if __name__ == '__main__':
+    res = ReadCSV()
+    print(res.to_str())
+    print(res.system_max())
+    print(res.max_rows())
