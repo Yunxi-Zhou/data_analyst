37a095981818fc93d5ff221f102b2f042aafbda7
Yunxi-Zhou <zhouyunxi1115@gmail.com>
2025-08-15 13:08:04 -0400

    cleaning wrong data

--- my_pandas/fixdata.py
+++ my_pandas/fixdata.py
@@ -3,4 +3,30 @@ import pandas as pd
 class FixData:
     def __init__(self):
         self.sep = '-----------------------------'
+        self.data = 'data/sample.csv'
     
+    def ini_read(self,data):
+        print(self.sep)
+        return pd.read_csv(data)
+    
+    def read_csv(self):
+        return self.ini_read(self.data)
+    
+    def replace_value(self,row:int,attributes:str,value):
+        df = self.ini_read(self.data)
+        df.loc[row,attributes] = value
+        return df
+    
+    def replace_loop(self,attributes:str,value:int):
+        df = self.ini_read(self.data)
+        for x in df.index:
+            if df.loc[x, attributes] > value:
+                df.loc[x, attributes] = value
+        return df
+
+    def remove_row(self,attributes:str,value:int):
+        df = self.ini_read(self.data)
+        for x in df.index:
+            if df.loc[x, attributes] > value:
+                df.drop(x, inplace=True)
+        return df
