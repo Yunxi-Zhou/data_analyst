ac8254604d2db20392a9945442c94881091437d6
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2025-08-18 17:06:39 -0400

    return plot

--- my_pandas/plotting.py
+++ my_pandas/plotting.py
@@ -5,3 +5,15 @@ class Plotting:
     def __init__(self):
         self.sep = '--------------------------------'
         self.data = 'data/sample2.csv'
+        
+    def ini_read(self,data):
+        print(self.sep)
+        return pd.read_csv(data)
+    
+    def read_csv(self):
+        return self.ini_read(self.data)
+    
+    def return_plot(self):
+        df = self.ini_read(self.data)
+        df.plot()
+        plt.show()
