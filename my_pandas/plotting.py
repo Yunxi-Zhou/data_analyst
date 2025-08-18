4a41fb6790f2f6f84ec50559b35d253194d73218
Yunxi-Zhou <zhouyunxi1115@gmail.com>
2025-08-18 16:39:36 -0400

    plotting

---
+++ my_pandas/plotting.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class Plotting:
+    def __init__(self):
+        self.sep = '--------------------------------'
+        self.data = 'data/sample2.csv'
