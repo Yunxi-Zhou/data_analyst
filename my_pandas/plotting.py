fbf4e96ff2e66029cb0d9cededa0dafb7354b492
Yunxi-Zhou <zhouyunxi1115@gmail.com>
2025-08-18 17:42:28 -0400

    combine with matplotlib

--- my_pandas/plotting.py
+++ my_pandas/plotting.py
@@ -3,11 +3,9 @@ import matplotlib.pyplot as plt
 
 class Plotting:
     def __init__(self):
-        self.sep = '--------------------------------'
         self.data = 'data/sample2.csv'
         
     def ini_read(self,data):
-        print(self.sep)
         return pd.read_csv(data)
     
     def read_csv(self):
@@ -16,4 +14,15 @@ class Plotting:
     def return_plot(self):
         df = self.ini_read(self.data)
         df.plot()
+        plt.show()
+
+    def scatter_plot(self,x,y):
+        df = self.ini_read(self.data)
+        df.plot(kind='scatter',x=x,y=y)
+        plt.show()
+    
+    def plot_attr(self,attr,kind):
+        df = self.ini_read(self.data)
+        df[attr].plot(kind=kind)
+        plt.title(f"{attr}({kind})")
         plt.show()
