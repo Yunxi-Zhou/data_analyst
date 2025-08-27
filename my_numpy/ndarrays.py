175b5777f0cab42e5bdf9c66ef2382080141ce33
Yunxi-Zhou <zhouyunxi1115@gmail.com>
2025-08-27 14:28:13 -0400

    creating, indexing and slicing arrays

---
+++ my_numpy/ndarrays.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class Ndarrays:
+    def __init__(self):
+        self.sep = '----------------'
+
+    def array_info(self,arr,ndmin=None):
+        print(self.sep)
+        arr_obj = np.array(arr) if ndmin is None else np.array(arr,ndmin=ndmin)
+        print(arr_obj)
+        print(f'dim: {arr_obj.ndim}')
+        return arr_obj, arr_obj.ndim
+    
+    def return_index(self,arr):
+        print(f"index is {arr}")
+
+    def return_slice(self,arr):
+        print(f"Slice element is {arr}")
