05365d32fbb34402a905592ebfe6101001ad0856
Yunxi-Zhou <zhouyunxi1115@gmail.com>
2025-08-28 13:23:08 -0400

    add .shape to return the shape of array, add attribute arr ndmin and dtype

--- my_numpy/basenumpy.py
+++ my_numpy/basenumpy.py
@@ -4,8 +4,15 @@ class BaseNumpy:
     def __init__(self):
         self.sep = '='*22
     
-    def ini_np(self,arr,dtype=None,return_print=True,return_dtype=False,return_arr=False):
-        a = np.array(arr) if dtype is None else np.array(arr,dtype=dtype)
+    def ini_np(self,arr=None,ndmin=None,dtype=None,return_print=True,return_dtype=False,return_arr=False):
+        if ndmin is None and dtype is None:
+            a = np.array(arr)
+        elif ndmin is None:
+            a = np.array(arr, dtype=dtype)
+        elif dtype is None:
+            a = np.array(arr, ndmin=ndmin)
+        else:
+            a = np.array(arr, ndmin=ndmin, dtype=dtype)
         #
         dt = np.asarray(arr).dtype
         kind = dt.kind
@@ -34,7 +41,7 @@ class BaseNumpy:
         elif kind == 'b':
             tname = "boolean"
         elif kind == 'S':
-            tname = f"byte string (ASCII, length={itemsize})"
+            tname = f"byte string (aSCII, length={itemsize})"
         elif kind == 'U':
             tname = f"Unicode string (max length={itemsize // 4})"
         elif kind == 'O':
@@ -46,8 +53,10 @@ class BaseNumpy:
         #
         if return_print:
             print(self.sep)
+            print('Data of array:')
             print(a)
-            print(f'Dim: {a.ndim}')
+            print(f'Dim of array: {a.ndim}')
+            print(f'Shape of array: {a.shape}')
         
         if return_dtype:
             print(f'Type of array: {np.array(a).dtype}')
