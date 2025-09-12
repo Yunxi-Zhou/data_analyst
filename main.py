6b449e9b41431b694bb4021612397cafe6bf8a53
Yunxi-Zhou <zhouyunxi1115@gmail.com>
2025-09-12 10:58:00 -0400

    current main for data_analyst

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-from my_pandas import Plotting
+from my_numpy import ArrayJoin
 
 if __name__ == '__main__':
-    res = Plotting()
+    arr1 = [1,2,3]
+    arr2 = [4,5,6]
+    res1 = ArrayJoin(arr1,arr2)
+    print('concatenate()')
+    res1.return_con()
+    arr3 = [[1, 2], [3, 4]]
+    arr4 = [[5, 6], [7, 8]]
+    res2 = ArrayJoin(arr3,arr4)
+    res2.return_con()
+    res2.return_con(axis=1)
+    print('stack()')
+    res1.return_stack()
+    res1.return_stack(axis=1)
+    print('hstack()')
+    res1.return_hstack()
+    print('vstack()')
+    res1.return_vstack()
+    print('dstack()')
+    res1.return_dstack()
